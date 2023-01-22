cd453a5bfcf34940085ad48ecfa8c55514531623
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-01-22 20:08:09 +0700

    add routing for the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Quiz from './pages/Quiz';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import ProtectedRoute, { useLocalStorage } from './utils/ProtectedRoute';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+  const { user } = useLocalStorage();
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route
+          path='/quiz'
+          element={
+            <ProtectedRoute user={user}>
+              <div className='m-14'>
+                <Quiz />
+              </div>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
